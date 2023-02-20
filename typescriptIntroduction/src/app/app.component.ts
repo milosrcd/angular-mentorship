5233979f3b839b96107de04935178b27ebdc3023
Milos Radosavljevic <milosrcd@gmail.com>
2023-02-20 14:32:11 +0100

    feat: Candidate class – method for print information

--- typescriptIntroduction/src/app/app.component.ts
+++ typescriptIntroduction/src/app/app.component.ts
@@ -4,24 +4,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'typescriptIntroduction';
 
+  candidate1 = new Candidate('Marko', 'Petrovic', new Date(1987, 1, 1));
+  candidate2 = new Candidate('Aleksandar', 'Djordjevic', new Date(1989, 2, 2));
+
+  arrayOfCandidates: Candidate[] = [
+    {
+      firstName: 'Milos',
+      lastName: 'Radosavljevic',
+      birthday: new Date(1987, 3, 4),
+    },
+    {
+      firstName: 'Tamara',
+      lastName: 'Manojlovic',
+      birthday: new Date(1987, 0, 4),
+    },
+  ];
+
   ngOnInit(): void {
-    const candidate1 = new Candidate('Marko', 'Petrovic', new Date(1987, 1, 1));
-    const candidate2 = new Candidate('Aleksandar', 'Djordjevic', new Date(1989, 2, 2));
+    console.log(this.candidate1);
+    console.log(this.candidate2);
 
-    console.log(candidate1);
-    console.log(candidate2);
-
-    const arrayOfCandidates: Candidate[] = [
-      {"firstName": 'Milos', "lastName": 'Radosavljevic', "birthday": new Date(1987, 1, 4)},
-      {"firstName": 'Tamara', "lastName": 'Manojlovic', "birthday": new Date(1987, 4, 1)}
-    ];
-
-    console.log(arrayOfCandidates);
+    console.log(this.arrayOfCandidates);
+    const candidateString = this.printCandidates(this.arrayOfCandidates);
+    console.log(candidateString);
   }
 
+  printCandidates(candidates: Candidate[]): string {
+    return candidates
+      .map(
+        (candidate) =>
+          `${candidate.firstName}` +
+          ' ' +
+          `${candidate.lastName}` +
+          ' : ' +
+          `${candidate.birthday.toDateString()}`
+      )
+      .join(' \n');
+  }
 }
