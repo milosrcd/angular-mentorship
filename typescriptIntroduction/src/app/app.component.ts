5eaf5a992dc9c1bd599f081d09ae348b6d330510
Milos Radosavljevic <milosrcd@gmail.com>
2023-02-20 10:40:07 +0100

    feat: Candidate class

--- typescriptIntroduction/src/app/app.component.ts
+++ typescriptIntroduction/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Candidate } from './models/candidate.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'typescriptIntroduction';
+
+  ngOnInit(): void {
+    const candidate1 = new Candidate('Marko', 'Petrovic', new Date(1987, 1, 1));
+    const candidate2 = new Candidate('Aleksandar', 'Djordjevic', new Date(1989, 2, 2));
+
+    console.log(candidate1);
+    console.log(candidate2);
+  }
+
 }
