7520ca2b5573a4505ef594894ea587000d6b20f2
Milos Radosavljevic <milosrcd@gmail.com>
2023-02-27 13:51:51 +0100

    feat: HTML structure

--- AngularModulesComponentsdatabinding/src/app/app.module.ts
+++ AngularModulesComponentsdatabinding/src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CoreModule, SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularModulesComponentsdatabinding/src/app/shared/shared.module.ts
+++ AngularModulesComponentsdatabinding/src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagesComponent } from './components/pagenav/pages.component';
+import { BookComponent } from './components/book/book.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { SearchComponent } from './components/search/search.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagesComponent,
+    BookComponent,
+    WrapperComponent,
+    SearchComponent,
+    FilterComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [PagesComponent, BookComponent, WrapperComponent, SearchComponent, FilterComponent]
 })
 export class SharedModule { }
