6dc58c28a692ce40ff373b7571ea44197f8d730b
Milos Radosavljevic <milosrcd@gmail.com>
2023-03-14 21:43:58 +0100

    Feat: sidenav added-not finished

--- AngularModulesComponentsdatabinding/src/app/core/core.module.ts
+++ AngularModulesComponentsdatabinding/src/app/core/core.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FeaturesModule } from "../features/features.module";
 
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [HeaderComponent, FooterComponent]
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        SidebarComponent,
+    ],
+    exports: [HeaderComponent, FooterComponent, SidebarComponent],
+    imports: [
+        CommonModule, MatSidenavModule,
+        FeaturesModule
+    ]
 })
 export class CoreModule { }
