1f018a907857b63657c7c31255561db5a3a696a5
Milos Radosavljevic <milosrcd@gmail.com>
2023-03-25 18:42:35 +0100

    Fix: Single book component copied do features routing module

--- AngularModulesComponentsdatabinding/src/app/app-routing.module.ts
+++ AngularModulesComponentsdatabinding/src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
-import { SingleBookComponent } from './features/single-book/single-book.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WrapperComponent } from './features/wrapper/wrapper.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('../app/features/features.module').then((module) => module.FeaturesModule),
-  },
-  {
-    path: 'book/:bookId',
-    component: SingleBookComponent
   },
   {
     path: 'admin',
--- AngularModulesComponentsdatabinding/src/app/features/features-routing.module.ts
+++ AngularModulesComponentsdatabinding/src/app/features/features-routing.module.ts
@@ -1,12 +1,17 @@
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { SingleBookComponent } from './single-book/single-book.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WrapperComponent
-  }
+  },
+  {
+    path: 'book/:bookId',
+    component: SingleBookComponent
+  },
 ];
 
 @NgModule({
