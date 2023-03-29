4a848c2ca2607dcb8c0db786db685aa57df0a7d8
Milos Radosavljevic <milosrcd@gmail.com>
2023-03-29 11:26:30 +0200

    1. Create an observable to emit the current time every second (interval)

--- angular-RXJS/src/app/app.component.ts
+++ angular-RXJS/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  currentTime$ = interval(1000);
+
+  ngOnInit(): void {
+
+  }
+
+  trackTimer() {
+    this.currentTime$.subscribe(() =>
+      console.log(new Date()));
+  }
 }
