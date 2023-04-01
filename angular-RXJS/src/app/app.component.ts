a7699c0bbfe6d2fdff9bfe0341255b0a1cfe1aee
Milos Radosavljevic <milosrcd@gmail.com>
2023-04-01 11:11:14 +0200

    Feat: 5. Create an observable to emit the sum of two input fields (map)

--- angular-RXJS/src/app/app.component.ts
+++ angular-RXJS/src/app/app.component.ts
@@ -11,7 +11,11 @@ export class AppComponent implements OnInit {
 
   currentTime$ = interval(1000);
   searchInput$!: Observable<string>;
-  values = '';
+  input1$!: Observable<string>;
+  input2$!: Observable<string>;
+  result$!: Observable<string[]>;
+  input3$!: Observable<number>;
+  input4$!: Observable<number>;
 
   data1$ = ajax('https://www.boredapi.com/api/activity?participants=1');
   data2$ = ajax('https://www.boredapi.com/api/activity?type=recreational');
@@ -36,15 +40,46 @@ export class AppComponent implements OnInit {
     this.searchInput$.subscribe((data: string) => {
       console.log('input value: ', data);
     })
+
+    this.input1$ = fromEvent(document.getElementById('input1')!, 'keyup').pipe(
+      map((data: Event) => {
+        return ((data.target as HTMLInputElement).value)
+      })
+    );
+    this.input2$ = fromEvent(document.getElementById('input2')!, 'keyup').pipe(
+      map((data: Event) => {
+        return ((data.target as HTMLInputElement).value)
+      })
+    );
+
+    this.result$ = combineLatest([this.input1$, this.input2$]);
+    this.result$.subscribe((data: string[]) => {
+      console.log(data)
+    })
+
+    this.input3$ = fromEvent(document.getElementById('input3')!, 'keyup').pipe(
+      map((data: Event) => {
+        return Number((data.target as HTMLInputElement).value);
+      })
+    )
+    this.input4$ = fromEvent(document.getElementById('input4')!, 'keyup').pipe(
+      map((data: Event) => {
+        return Number((data.target as HTMLInputElement).value);
+      })
+    )
+
+    const result2$ = combineLatest([this.input3$, this.input4$]).pipe(
+      map(([input1, input2]) => input1 + input2)
+    );
+    result2$.subscribe((result2$) => {
+      console.log(`Sum of input3 and input4 is: ${result2$}`);
+    })
   }
+
 
   trackTimer() {
     this.currentTime$.subscribe(() =>
       console.log(new Date()));
-  }
-
-  onEnter(value: string) {
-    console.log(this.values = value);
   }
 
   forkJoinApis() {
