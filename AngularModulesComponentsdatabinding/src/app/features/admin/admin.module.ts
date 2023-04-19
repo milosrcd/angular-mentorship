d1f26421433038845128fea48e5169d2bb1a58ca
Milos Radosavljevic <milosrcd@gmail.com>
2023-04-19 22:46:38 +0200

    Feat: just added angular material table with paginator

--- AngularModulesComponentsdatabinding/src/app/features/admin/admin.module.ts
+++ AngularModulesComponentsdatabinding/src/app/features/admin/admin.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AdminBookComponent } from './components/admin-book/admin-book.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-
-
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     AdminRoutingModule,
-    SharedModule
+    SharedModule,
+    MatPaginatorModule,
+    MatTableModule
   ]
 })
 export class AdminModule { }
