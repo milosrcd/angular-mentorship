928c881864000a900567afa8b7229d50a57100ea
Milos Radosavljevic <milosrcd@gmail.com>
2023-04-19 10:14:20 +0200

    Fix: #issueid58

--- AngularModulesComponentsdatabinding/src/app/features/components/singlebook/singlebook.component.ts
+++ AngularModulesComponentsdatabinding/src/app/features/components/singlebook/singlebook.component.ts
@@ -1,20 +1,23 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { BookDetails } from 'src/app/features/models/book-details.model';
 import { Category } from 'src/app/features/models/category.enum';
 import { BookService } from '../../services/book.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-singlebook',
   templateUrl: './singlebook.component.html',
   styleUrls: ['./singlebook.component.scss']
 })
-export class SingleBookComponent implements OnInit {
+export class SingleBookComponent implements OnInit, OnDestroy {
 
   bookId: string | null = '';
   singleBook?: BookDetails;
 
   books: BookDetails[] = [];
+
+  private unsubscribe$: Subject<void> = new Subject<void>;
 
   constructor(private activatedRouter: ActivatedRoute, private bookService: BookService) { }
 
@@ -31,10 +34,19 @@ export class SingleBookComponent implements OnInit {
   }
 
   private getBookId() {
-    this.activatedRouter.paramMap.subscribe(params => {
+    this.activatedRouter.paramMap
+    .pipe(
+      takeUntil(this.unsubscribe$)
+    )
+    .subscribe(params => {
       const id = params.get('id');
       this.getBookById(id);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
