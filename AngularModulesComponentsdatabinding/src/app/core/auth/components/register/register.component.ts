0f8b5ae25284fa84cb331d0e8599a732d0a20d48
Milos Radosavljevic <milosrcd@gmail.com>
2023-05-01 20:51:38 +0200

    Fix: some field fixes

--- AngularModulesComponentsdatabinding/src/app/core/auth/components/register/register.component.ts
+++ AngularModulesComponentsdatabinding/src/app/core/auth/components/register/register.component.ts
@@ -15,8 +15,8 @@ export class RegisterComponent implements OnInit {
     firstName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Z][a-z]*$/)]),
     lastName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Z][a-z]*$/)]),
     email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    password: new FormControl('', Validators.required),
-    confirmPassword: new FormControl('', [Validators.required, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^.{8,}$/)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.pattern(/^.{8,}$/)]),
     countries: new FormControl('', Validators.required),
   });
 
